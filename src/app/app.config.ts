ec8671042662fc9d26f9d39e4c354cc4776a56e9
RKCbas <sebashp2015@hotmail.com>
2025-05-15 16:46:15 -0600

    # Class 6 ✨Feat: Created a service that uses a httpClient that its provided in the app.config and show in the console the response of the request to the API_URL

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 // import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
@@ -13,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     //   provide: LocationStrategy,
     //   useClass: HashLocationStrategy
     // },
+    provideHttpClient(withFetch())
   ]
 };
--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'by-capital-page',
@@ -13,9 +14,12 @@ import { SearchInputComponent } from "../../components/search-input/search-input
 })
 export default class ByCapitalPageComponent {
 
+  countryService = inject(CountryService)
 
-  handleOnSearch(value:string){
-    console.log({value})
+  handleOnSearch(query:string){
+    this.countryService.searchByCapital(query).subscribe( resp => {
+      console.log({resp})
+    })
   }
 
 }
