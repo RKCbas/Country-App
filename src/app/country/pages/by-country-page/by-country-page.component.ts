2aa8b1c1c53578709c9891791dcbd48060214255
RKCbas <sebashp2015@hotmail.com>
2025-05-19 14:51:21 -0600

    # Homework ✨Feat: implement the changes to the by-country-page

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,32 +1,42 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryService } from '../../services/country.service';
 import { of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'by-country-page',
   imports: [
-    CountryListComponent, 
+    CountryListComponent,
     SearchInputComponent
   ],
   templateUrl: './by-country-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ByCountryPageComponent { 
-  
+export default class ByCountryPageComponent {
+
   countryService = inject(CountryService);
-  query = signal('');
+
+  router = inject(Router);
+
+  activatedRoute = inject(ActivatedRoute);
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? ''
+
+  query = linkedSignal(() => this.queryParam);
 
   countryResource = rxResource({
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
 
-      if(!request.query) return of([]);
+      if (!request.query) return of([]);
+
+      this.router.navigate(['country/by-country'], {
+        queryParams: { query: request.query }
+      });
 
       return this.countryService.searchByCountry(request.query)
-      
     }
   });
 
